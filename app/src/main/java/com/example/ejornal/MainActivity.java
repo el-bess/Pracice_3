e9f2117e824040184e3ac8c9a6bfa77af6460d0b
ModinaLiza <lizoklove85@gmail.com>
2023-03-18 18:08:55 +0300

    4 commit (third practice)

--- app/src/main/java/com/example/ejornal/MainActivity.java
+++ app/src/main/java/com/example/ejornal/MainActivity.java
@@ -24,43 +24,6 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .add(R.id.container_third, third_list.class, null)
-                    .commit();
-        }
-
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-
-        hobbyName=binding.showHobby;
-        editText = binding.editName;//Ввод логина
-        binding.btEnter.setOnClickListener(onclBtEnter);
-
-        binding.btAutor.setOnClickListener(onclBtAutor);//Кнопка авторизироваться
-    }
-    View.OnClickListener onclBtEnter=new View.OnClickListener() {
-        @Override
-        public void onClick(View v){
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            intent.putExtra("name", editText.getText().toString());
-            startActivityForResult(intent,1);
-        }
-    };
-    View.OnClickListener onclBtAutor=new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(MainActivity.this, Activity3.class);
-            startActivity(intent);
-        }
-    };
-    protected void onActivityResult(int recCode, int resCode, Intent data) {
-
-        super.onActivityResult(recCode, resCode, data);
-        if (data == null){
-            return;
-        } String name = data.getStringExtra("name");
-        hobbyName.setText("В прошлый раз вы выбрали это хоббби: "+name+". Что будет сегодня?");
 
     }
 
---
+++ app/src/main/java/com/example/ejornal/first_list.java
@@ -0,0 +1,18 @@
+package com.example.ejornal;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class first_list extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.first_list, container, false);
+    }
+}
