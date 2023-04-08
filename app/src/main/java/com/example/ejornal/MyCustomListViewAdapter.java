d96c83917228863e6568e23b12cfad129c4e5064
ModinaLiza <lizoklove85@gmail.com>
2023-04-08 16:35:40 +0300

    2 commit (forth practice)

---
+++ app/src/main/java/com/example/ejornal/MyCustomListViewAdapter.java
@@ -0,0 +1,36 @@
+package com.example.ejornal;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyCustomListViewAdapter extends ArrayAdapter<String> {
+    private LayoutInflater inflater;
+    private int layout;
+    private List<String> schools;
+
+    public MyCustomListViewAdapter(Context context, int resource, List<String> schools) {
+        super(context, resource, schools);
+        this.schools = schools;
+        this.layout = resource;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = inflater.inflate(this.layout, parent, false);
+
+        TextView text_school = view.findViewById(R.id.text_school);
+        text_school.setText(schools.get(position));
+
+        ImageView image_school = view.findViewById(R.id.image_school);
+        image_school.setImageResource(R.drawable.cap);
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/ejornal/sec_list.java
+++ app/src/main/java/com/example/ejornal/sec_list.java
@@ -1,24 +1,59 @@
 package com.example.ejornal;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
+import com.example.ejornal.databinding.FragmentSecListBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class sec_list extends Fragment {
-
+    FragmentSecListBinding binding;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-    public sec_list(){
+
+    public sec_list() {
         super(R.layout.fragment_sec_list);
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentSecListBinding.inflate(inflater, container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        List<String> hobby_arr = new ArrayList<String>();
+        for (int i = 0; i < 300; i++){
+            hobby_arr.add("Кружки " + (i+1));
+        }
+
+        MyCustomListViewAdapter listViewAdapter = new MyCustomListViewAdapter(getActivity(), R.layout.itime_sec, hobby_arr);
+        binding.listView.setAdapter(listViewAdapter);
+        binding.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getContext(), hobby_arr.get(i), Toast.LENGTH_SHORT).show();
+                Log.i("ListView", hobby_arr.get(i));
+            }
+        });
+    }
+
+
 }
