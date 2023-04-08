18f8fa6a6d52054c15456fa3d84a16ece010f0bd
ModinaLiza <lizoklove85@gmail.com>
2023-04-08 14:05:27 +0300

    2 commit (forth practice)

--- app/src/main/java/com/example/ejornal/first_list.java
+++ app/src/main/java/com/example/ejornal/first_list.java
@@ -1,12 +1,18 @@
 package com.example.ejornal;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.ejornal.databinding.FirstListBinding;
+
 public class first_list extends Fragment {
+    FirstListBinding binding;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +23,29 @@ public class first_list extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.first_list, container, false);
+        binding = FirstListBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+    public void onViewCreated( View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FragmentManager fragmentManager = getParentFragmentManager();
+        binding.btEnter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction;
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_view, new sec_list());
+                fragmentTransaction.commit();
+            }
+        });
+
+        binding.btAutor.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction;
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_view, new third_list());
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
