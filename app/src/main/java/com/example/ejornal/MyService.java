4ddb5bb23e5f7ea17dadcf616cdf3c39c01a309c
ModinaLiza <lizoklove85@gmail.com>
2023-05-06 10:45:07 +0300

    7 practice - begining

---
+++ app/src/main/java/com/example/ejornal/MyService.java
@@ -0,0 +1,77 @@
+package com.example.ejornal;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Build;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
+
+    private WindowManager mWindowManager;
+    private View mBannerView;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mBannerView = LayoutInflater.from(this).inflate(R.layout.service_layout, null);
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.TOP | Gravity.CENTER_HORIZONTAL;
+        params.y = 100;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            Intent intent2 = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent2);
+        }
+        else {
+            mWindowManager.addView(mBannerView, params);
+        }
+
+        Intent appIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, appIntent, 0);
+
+        mBannerView.setOnClickListener(v -> {
+            try {
+                pendingIntent.send();
+                onDestroy();
+            } catch (PendingIntent.CanceledException e) {
+                e.printStackTrace();
+            }
+        });
+
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (mBannerView != null) {
+            mWindowManager.removeView(mBannerView);
+        }
+    }
+}
